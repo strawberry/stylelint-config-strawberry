4c3202d5c5ef3fc03bdddee7d5f04f79ecbf94f6
davidwarrington <davidmwarrington+github@gmail.com>
2020-04-22 16:53:27 +0100

    Set up initial Stylelint config

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+      'stylelint-scss'
+    ],
+    rules: {
+      'at-rule-no-unknown': null,
+      indentation: 4,
+      'scss/at-rule-no-unknown': true,
+      'selector-list-comma-newline-after': 'always',
+      'selector-max-id': 0
+    }
+}
