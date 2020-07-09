3119058c2ae54d9052e00c3f77a6d6743fdc8931
davidwarrington <davidmwarrington+github@gmail.com>
2020-07-09 15:53:08 +0100

    Enforce alphabetical declaration order.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
     plugins: [
-      'stylelint-scss'
+      'stylelint-scss',
+      'stylelint-order'
     ],
     rules: {
       'at-rule-no-unknown': null,
       indentation: 4,
+      'order/properties-alphabetical-order': true,
       'scss/at-rule-no-unknown': true,
       'selector-list-comma-newline-after': 'always',
       'selector-max-id': 0
